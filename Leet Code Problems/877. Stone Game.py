53edb10c860a123f549ac87ff51d06c886b23ebe
Akshay Anandrao Jagadale <aksbret1990@gmail.com>
2018-09-27 22:47:22 -0700

    Add files via upload

---
+++ Leet Code Problems/877. Stone Game.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 27 18:59:23 2018
+
+@author: Akshay Jagadale
+"""
+
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        
+        return True
+        
+        
+myobj = Solution()
+myobj.stoneGame([5,3,4,5])
